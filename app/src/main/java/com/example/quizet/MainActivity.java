afb658fe970be7a548fa1aee9a6bd698e1624e91
nabilChemkhi <nabil.chemkhi@esprit.tn>
2021-10-18 19:18:12 +0100

    MainActivity

--- app/src/main/java/com/example/quizet/MainActivity.java
+++ app/src/main/java/com/example/quizet/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.quizet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView title;
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title=findViewById(R.id.mainTitleId);
+        startBtn=findViewById(R.id.startBtnId);
+
+        //Typeface typeFace = ResourcesCompat.getFont(this,R.font.blackList);
+        //title.setTypeface(typeFace);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
