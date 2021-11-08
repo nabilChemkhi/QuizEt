ca391f7a040ebdcef5f07d76853723ac0faf7c65
nabilChemkhi <nabil.chemkhi@esprit.tn>
2021-11-08 16:10:24 +0100

    activity chge

--- app/src/main/java/com/example/quizet/QuestionsActivity.java
+++ app/src/main/java/com/example/quizet/QuestionsActivity.java
@@ -205,7 +205,7 @@ public class QuestionsActivity extends AppCompatActivity implements View.OnClick
                                 case 4:
                                     ((Button) view).setText(questionList.get(curentQust).getOptionD());
                                     break;
-                                //default://
+                                //default:
                             }
                             if(numView != 0)
                                // ((Button)view).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("0000FF")));
